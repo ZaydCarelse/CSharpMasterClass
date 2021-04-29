a9c849981e7fc1dbc64e9b583a259131fae89034
Zayd Carelse <zayd.carelse@gmail.com>
2021-04-29 21:17:14 +0200

    Tested program with Parsing logic

--- TestProgram.cs
+++ TestProgram.cs
@@ -6,16 +6,14 @@ namespace Test_Application
     {
         static void Main(string[] args)
         {
-            int num1 = 32;
-            int num2 = 156;
-            int sum = num1 + num2;
+            string myString = "156";
+            string mySecondString = "13";
 
-            string message = "This is a string to test the program with.";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
 
-            string messagePlus = "This is another message.";
-
-            Console.WriteLine("The sum of " + num1 + " and " + num2 +" is " + sum);
-            Console.WriteLine(message.ToUpper() + " " + messagePlus);
+            int result = num1 + num2;
+            Console.WriteLine("The result of the addition is : " + result);
         }
     }
 }
