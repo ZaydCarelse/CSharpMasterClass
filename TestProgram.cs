edd18eade248bfe29ec86aa291e7327fc93154fd
Zayd Carelse <zayd.carelse@gmail.com>
2021-04-30 00:23:29 +0200

    Finished Exercise: Strings 2

--- TestProgram.cs
+++ TestProgram.cs
@@ -7,15 +7,27 @@ namespace Test_Application
         static void Main(string[] args)
         {
             string myString;
+            string searchChar;
             
-            Console.WriteLine("Please enter your name and press enter");
+            Console.Write("Enter string here: ");
             myString = Console.ReadLine();
             
-            Console.WriteLine(myString.ToUpper());
-            Console.WriteLine(myString.ToLower());
-            Console.WriteLine(myString.Trim());
-            Console.WriteLine(myString.Substring(6));
+            Console.Write("Enter the character to search for here: ");
+            searchChar = Console.ReadLine();
+            
+            Console.WriteLine(myString.IndexOf(searchChar));
 
+            string firstName;
+            string lastName;
+            
+            Console.Write("Please enter your first name: ");
+            firstName = Console.ReadLine();
+            
+            Console.Write("Please enter your last name: ");
+            lastName = Console.ReadLine();
+
+            string fullName = string.Concat(" ",firstName, lastName, " ");
+            Console.WriteLine("Your full name is:" + fullName);
         }
     }
 }
